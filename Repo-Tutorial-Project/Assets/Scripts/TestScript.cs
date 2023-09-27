7d57730311dc39d0fd15c60a4cf82ecec16a5365
Conner Steedman <144230588+Pasagow@users.noreply.github.com>
2023-09-27 12:51:59 +0100

    Added variables for console logs

--- Repo-Tutorial-Project/Assets/Scripts/TestScript.cs
+++ Repo-Tutorial-Project/Assets/Scripts/TestScript.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    string combinedString;
+    [SerializeField]
+    string editorString = "KAGURA BACHI RAGHHHHHH";
     // Start is called before the first frame update
     void Start()
     {
+        /*
         Debug.Log("You have my sword");
         Debug.Log("And my bow");
         Debug.Log("And my axe!");
+        */
 
+        //declare variable FirstString
+        string firstString = "You shall not";
+        string secondString = " PASS!";
+        
+        combinedString = firstString + secondString;
+
+        Debug.Log(combinedString);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Test,Test,Test");
-        transform.Translate(0.1f, 0, 0);
+        //Debug.Log("Test,Test,Test");
+        //transform.Translate(0.1f, 0, 0);
+        Debug.Log(editorString);
     }
 }
